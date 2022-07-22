e132593d27bd43cf2a0f8217868093e7dbd825b6
yuhcee <egbouchenna001@gmail.com>
2022-07-22 18:28:01 +0100

    feat(Venue):insert form data as a new Venue record in the db

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
-from models import app, Artist, Venue, Show, format_datetime
+from models import app, db, Artist, Venue, Show, format_datetime
 
 #----------------------------------------------------------------------------#
 # Controllers.
@@ -20,9 +20,9 @@ from models import app, Artist, Venue, Show, format_datetime
 def index():
     return render_template('pages/home.html')
 
-
 #  Venues
 #  ----------------------------------------------------------------
+
 
 @app.route('/venues')
 def venues():
@@ -165,14 +165,38 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
 
-    # on successful db insert, flash success
-    flash('Venue ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    if "seeking_talent" in request.form and request.form['seeking_talent'] == 'y':
+        seeking_talent = True
+        seeking_description = request.form['seeking_description']
+    else:
+        seeking_talent = False
+        seeking_description = ''
+
+    try:
+        venue = Venue(
+            name=request.form['name'],
+            city=request.form['city'],
+            state=request.form['state'],
+            address=request.form['address'],
+            phone=request.form['phone'],
+            genres=request.form.getlist('genres'),
+            image_link=request.form['image_link'],
+            facebook_link=request.form['facebook_link'],
+            website=request.form['website_link'],
+            seeking_talent=seeking_talent,
+            seeking_description=seeking_description
+        )
+        db.session.add(venue)
+        db.session.commit()
+        flash('Venue ' + request.form['name'] + ' was successfully created!')
+    except Exception as e:
+        print(e)
+        flash('An error occurred. Venue ' +
+              request.form['name'] + ' could not be created')
+        db.session.rollback()
+    finally:
+        db.session.close()
     return render_template('pages/home.html')
 
 
--- models.py
+++ models.py
@@ -60,7 +60,7 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_description = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default=False)
-    shows = db.relationship('Show', backref='venue', lazy=True)
+    shows = db.relationship('Show', backref='artist', lazy=True)
 
     def __repr__(self):
         return f'<Artist {self.id} {self.name}>'
