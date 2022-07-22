81df0818b458f6998c06f0d7636c65d9ec30979e
yuhcee <egbouchenna001@gmail.com>
2022-07-22 19:07:32 +0100

    feat(Show):add new shows in the db

--- app.py
+++ app.py
@@ -480,14 +480,22 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-    # called to create new shows in the db, upon submitting new show listing form
-    # TODO: insert form data as a new Show record in the db, instead
+    try:
+        show = Show(
+            artist_id=request.form['artist_id'],
+            venue_id=request.form['venue_id'],
+            start_time=request.form['start_time']
+        )
+        db.session.add(show)
+        db.session.commit()
+        flash('Show was successfully listed!')
+    except Exception as e:
+        print(e)
+        flash('An error occurred. Show could not be listed')
+        db.session.rollback()
+    finally:
+        db.session.close()
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
     return render_template('pages/home.html')
 
 
