2a4a8e05ff006d153576c12ee967abdc654a8ec6
yuhcee <egbouchenna001@gmail.com>
2022-07-22 21:06:39 +0100

    chore(Venue):populate form with values from venue with ID <venue_id>

--- app.py
+++ app.py
@@ -357,21 +357,20 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
+    venue = Venue.query.filter(Venue.id == venue_id).first()
+
+    form.city.data = venue.city
+    form.name.data = venue.name
+    form.phone.data = venue.phone
+    form.state.data = venue.state
+    form.genres.data = venue.genres
+    form.address.data = venue.address
+    form.website_link.data = venue.website
+    form.image_link.data = venue.image_link
+    form.facebook_link.data = venue.facebook_link
+    form.seeking_talent.data = venue.seeking_talent
+    form.seeking_description.data = venue.seeking_description
+
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
